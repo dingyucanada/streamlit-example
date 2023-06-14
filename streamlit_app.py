9e1c23a974685f6cdde32cb0fc3f71a3f674598b
dingyucanada <dingyucanada@gmail.com>
2023-06-14 10:35:53 -0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,48 +1,52 @@
+import sys
+import pandas as pd
 import numpy as np
-import pandas as pd 
-from sklearn import datasets
+import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_samples, silhouette_score
-import matplotlib.cm as cm
-from sklearn.metrics.cluster import adjusted_rand_score
 
-(X, y) = datasets.load_wine(return_X_y=True)
+def LRGradDesc(data, target, init_x1=0, init_x2=0, init_b=0,
+    learning_rate_w=0.001, learning_rate_b=0.1, max_iter=10000):
 
-k_min = 2
-k_max = 6
-clrs = ['red', 'green', 'blue', 'yellow']
-clr_i = 0
-inits = ['kmeans++', 'random']
-silhouette_avgs = []
-adjusted_rand = []
-for i in inits:
-    for k in range(k_min, k_max):
-        
-        km = KMeans(
-            n_clusters=k, init='random'
-        )
+    sns.set(style="whitegrid",rc={'figure.figsize':(11.7,8.27)})
+    tot_area = data.iloc[:,0] + data.iloc[:, 1]
+    plotting_data = pd.DataFrame({
+        'Total Square Feet': tot_area,
+        'Sale Price': target})
+    sns.scatterplot(x="Total Square Feet", y="Sale Price", data=plotting_data)
 
-        y_km = km.fit_predict(X)
+    print("\n### Training Model ###\n")
+    N = len(data.iloc[:, 0])
+    current_x1 = init_x1
+    current_x2 = init_x2
+    current_b = init_b
+    for i in range(max_iter):
+        prediction = np.dot(data.iloc[:,0], current_x1) + np.dot(data.iloc[:,1], current_x2) + current_b
 
-        # computing sillouette avg score
-        silhouette_avg = silhouette_score(X, y_km)
-        silhouette_avgs.append(silhouette_avg)
-        adjusted_rand.append(adjusted_rand_score(y, y_km))
+        diff = np.divide(prediction - target, target)
+        cost = np.sum(diff ** 2) / (2. * N)
 
-    sil_title = i + ": silhouette score"
-    adj_title = i + ": adjust rand index"
-    sil_clr = clrs[clr_i]
-    adj_clr = clrs[clr_i + 1]
-    plt.plot(range(k_min, k_max), silhouette_avgs, c=sil_clr, label=sil_title)
-    plt.plot(range(k_min, k_max), adjusted_rand, c=adj_clr, label=adj_title)
-    silhouette_avgs = []
-    adjusted_rand = []
-    clr_i += 2
+        x1_grad = np.dot((learning_rate_w/N), np.sum(np.dot(data.iloc[:,0], diff)))
+        x2_grad = np.dot((learning_rate_w/N), np.sum(np.dot(data.iloc[:,1], diff)))
+        b_grad = np.dot((learning_rate_b/N), np.sum(diff))
+    
+        current_x1 = current_x1 - x1_grad
+        current_x2 = current_x2 - x2_grad
+        current_b = current_b - b_grad
 
+        if i % 1000 == 0:
+            print("[Iteration " + str(i) + "] x1: " + str(current_x1) + " x2: " + str(current_x2) + " b: " + str(current_b) + " cost: " + str(cost))
 
-plt.legend()
-plt.xlabel('K')
-plt.ylabel('Score Metric')
-plt.suptitle("Scoring Metrics for KMeans Over K")
-plt.show()
+        if i == max_iter - 1:
+            print("\n### Final Results ###\n")
+            print("Final Cost: %.2f" % cost)
+            print("[Iteration " + str(i) + "] x1: " + str(current_x1) + " x2: " + str(current_x2) + " b: " + str(current_b) + " cost: " + str(cost))
+    
+    plt.plot(range(4500), np.dot(range(4500), current_x1) + np.dot(range(4500), current_x2) + current_b, color='red')
+    plt.show()
+    return current_x1, current_x2, current_b
+
+if __name__ == "__main__":
+    data_file = pd.read_csv("data-sets/housing-prices-dataset/train.csv", usecols=[43,44,80])
+    x1, x2, b = LRGradDesc(data_file.iloc[:,:2], data_file.iloc[:,2])
+
+    
